10ef6ef023f349677958b3fce57b182cad9b4166
まつ <contact.matsu@ma2kz.net>
2023-08-05 12:16:04 +0900

    Update Form1.vb
    v1

--- Form1.vb
+++ Form1.vb
@@ -1,70 +1,210 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+Imports System.Threading.Tasks
+Public Class Form1
 
+    Dim roulette As Integer = 0
+    Dim rcnt As Integer = 0
+    Dim rand As New System.Random()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 各項目の初期化
+    End Sub
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Button1.Enabled = False
 
-        Dim rand As New System.Random()
-        Dim arr() As Integer = {0, 0, 0}
+        ' --回転数--
+        Dim cnt As Integer
+        cnt = Val(Lab_cnt.Text)
+        Lab_cnt.Text = cnt + 1
+        ' ----------
 
-        Label1.Text = " " : Label2.Text = " "
-        Label3.Text = " " : Label4.Text = ""
+        ' --結果の計算--
+        Dim arr() As Integer = {0, 0, 0}
+        Lab_A0.Text = " " : Lab_A1.Text = " "
+        Lab_A2.Text = " " : Lab_st.Text = ""
 
         For i = 0 To 2
 
             arr(i) = rand.Next(1, 8)
 
         Next i
+        ' --------------
 
-        If arr(0) = 7 Then
-            Label1.ForeColor = Color.Gold
-        ElseIf arr(0) Mod 2 = 0 Then
-            Label1.ForeColor = Color.Blue
-        Else
-            Label1.ForeColor = Color.Red
-        End If
-        Label1.Text = arr(0)
+        ' --デバッグで結果操作--
+        ' arr = {1, 1, 1}
+        ' ----------------------
 
-        If arr(2) = 7 Then
-            Label3.ForeColor = Color.Gold
-        ElseIf arr(2) Mod 2 = 0 Then
-            Label3.ForeColor = Color.Blue
-        Else
-            Label3.ForeColor = Color.Red
-        End If
-        Label3.Text = arr(2)
-
-        If arr(0) = arr(2) Then Label4.Text = "　リーチ！"
-
-        If arr(1) = 7 Then
-            Label2.ForeColor = Color.Gold
-        ElseIf arr(1) Mod 2 = 0 Then
-            Label2.ForeColor = Color.Blue
-        Else
-            Label2.ForeColor = Color.Red
-        End If
-        Label2.Text = arr(1)
-
-        If arr(0) = arr(1) And arr(1) = arr(2) Then
-            If arr(0) Mod 2 = 0 Then
-                Label4.Text = "大当たり！"
-                Label4.ForeColor = Color.Blue
-            Else
-                Label4.Text = "大当たり！"
-                Label4.ForeColor = Color.Red
+        ' --天井--
+        If Val(Lab_cnt.Text) Mod 100 = 0 Then
+            If ToolStrip_100.Checked = True Then
+                roulette = rand.Next(1, 8)
+                For m = 0 To 2
+                    arr(m) = roulette
+                Next m
             End If
         End If
+        ' --------
+
+        ' --デバッグログ--
+        Debug.Write(Lab_cnt.Text & "回転：")
+        Debug.Write(String.Join(",", arr))
+        Debug.Write(" " & ToolStrip_100.Checked)
+        If arr(0) = arr(2) Then
+            If arr(0) = arr(1) Then
+                Debug.Write(" 当たり")
+            Else
+                Debug.Write(" リーチ")
+            End If
+        Else
+            End If
+        Debug.WriteLine("")
+        ' ----------------
+
+        ' --左リール決定--
+        Await A0()
+        If arr(0) = 7 Then
+            Lab_A0.ForeColor = Color.Gold
+        ElseIf arr(0) Mod 2 = 0 Then
+            Lab_A0.ForeColor = Color.Blue
+        Else
+            Lab_A0.ForeColor = Color.Red
+        End If
+        Lab_A0.Text = arr(0)
+        Await Task.Delay(1000)
+        ' ----------------
+
+        ' --右リール決定--
+        Await A2()
+        If arr(2) = 7 Then
+            Lab_A2.ForeColor = Color.Gold
+        ElseIf arr(2) Mod 2 = 0 Then
+            Lab_A2.ForeColor = Color.Blue
+        Else
+            Lab_A2.ForeColor = Color.Red
+        End If
+        Lab_A2.Text = arr(2)
+        ' ----------------
+
+        ' --リーチ判定--
+        Await Task.Delay(500)
+        If arr(0) = arr(2) Then
+            If arr(0) Mod 2 = 0 Then
+                Lab_st.ForeColor = Color.Blue
+            Else
+                Lab_st.ForeColor = Color.Red
+            End If
+            Lab_st.Text = "　リーチ！"
+            Await Task.Delay(1000)
+            rcnt = 39
+        Else
+            rcnt = 14
+        End If
+
+        ' --------------
+
+        ' --中リール決定--
+        Await A1()
+        If arr(1) = 7 Then
+            Lab_A1.ForeColor = Color.Gold
+        ElseIf arr(1) Mod 2 = 0 Then
+            Lab_A1.ForeColor = Color.Blue
+        Else
+            Lab_A1.ForeColor = Color.Red
+        End If
+        Lab_A1.Text = arr(1)
+        ' ----------------
+
+        ' --当選判定--
+        If Lab_st.Text = "　リーチ！" Then
+            If arr(0) = arr(1) Then
+                If arr(0) Mod 2 = 0 Then
+                    Lab_st.ForeColor = Color.Blue
+                    Lab_st.Text = "大当たり！"
+
+                Else
+                    Lab_st.ForeColor = Color.Red
+                    Lab_st.Text = "大当たり！"
+
+                End If
+            Else
+                Lab_st.ForeColor = Color.Black
+                Lab_st.Text = "　はずれ！"
+            End If
+        Else
+        End If
+        ' --------------
 
         Button1.Enabled = True
 
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Label1.Text = "7" : Label2.Text = "7"
-        Label3.Text = "7" : Label4.Text = ""
+    Private Async Function A0() As Task
 
-        Label1.ForeColor = Color.Gold
-        Label2.ForeColor = Color.Gold
-        Label3.ForeColor = Color.Gold
-        Label4.ForeColor = Color.Black
+        For j = 0 To 14
+
+            roulette = rand.Next(1, 8)
+
+            If roulette = 7 Then
+                Lab_A0.ForeColor = Color.Gold
+            ElseIf roulette Mod 2 = 0 Then
+                Lab_A0.ForeColor = Color.Blue
+            Else
+                Lab_A0.ForeColor = Color.Red
+            End If
+
+            Lab_A0.Text = roulette.ToString()
+            Await Task.Delay(50)
+
+        Next j
+
+    End Function
+
+    Private Async Function A1() As Task
+
+        For j = 0 To rcnt
+
+            roulette = rand.Next(1, 8)
+
+            If roulette = 7 Then
+                Lab_A1.ForeColor = Color.Gold
+            ElseIf roulette Mod 2 = 0 Then
+                Lab_A1.ForeColor = Color.Blue
+            Else
+                Lab_A1.ForeColor = Color.Red
+            End If
+
+            Lab_A1.Text = roulette.ToString()
+            Await Task.Delay(50)
+
+        Next j
+
+    End Function
+
+    Private Async Function A2() As Task
+
+        For j = 0 To 14
+
+            roulette = rand.Next(1, 8)
+
+            If roulette = 7 Then
+                Lab_A2.ForeColor = Color.Gold
+            ElseIf roulette Mod 2 = 0 Then
+                Lab_A2.ForeColor = Color.Blue
+            Else
+                Lab_A2.ForeColor = Color.Red
+            End If
+
+            Lab_A2.Text = roulette.ToString()
+            Await Task.Delay(50)
+
+        Next j
+
+    End Function
+
+    Private Sub ToolStrip_100_Click(sender As Object, e As EventArgs) Handles ToolStrip_100.Click
+        ToolStrip_100.Checked = Not ToolStrip_100.Checked
+    End Sub
+
+    Private Sub 終了ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 終了ToolStripMenuItem.Click
+        End
     End Sub
 End Class
